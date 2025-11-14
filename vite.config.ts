546c959c29a8d2ebbea234873e8dd28a6c1da855
kibum0405 <top5w5@naver.com>
2025-11-14 11:25:56 +0900

    도메인 관련 이슈 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/aimax-home.github.io/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
